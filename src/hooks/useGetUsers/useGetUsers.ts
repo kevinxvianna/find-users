8eda3f04b4e3ded601679dbeeb30a4f6fd4cb08a
Kevin Quinália <kevinvquinalia@gmail.com>
2024-09-24 15:38:35 -0300

    feat(search): Add new validation to case insensitive searches

--- src/hooks/useGetUsers/useGetUsers.ts
+++ src/hooks/useGetUsers/useGetUsers.ts
@@ -2,13 +2,14 @@ import { useQueryClient } from 'react-query';
 import { FETCH_USERS_KEY } from '../../constants';
 import { User } from '../../types';
 
-export const useGetUsers = (name: string) => {
+export const useGetUsers = (name: string): User[] | undefined => {
   const queryClient = useQueryClient();
+  const regex = new RegExp(name, 'i');
 
   const users = queryClient.getQueryData(FETCH_USERS_KEY) as User[] | undefined;
 
   if (!name) return users;
 
-  const foundUsers = users?.filter((user: User) => user.name.includes(name));
+  const foundUsers = users?.filter((user: User) => regex.test(user.name));
   return foundUsers;
 };
